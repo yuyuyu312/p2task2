4928e96731db86ef361cce9bf4d4e0ac0218f25b
yuyuyu312 <yusupov.ilgiz2323@gmail.com>
2019-06-13 23:35:29 +0600

    p2task2

---
+++ p2task2.py
@@ -0,0 +1,8 @@
+k = int(input("how many students?"))
+n = int(input("how many apples?"))
+int(a) = n / k
+x = k // n
+print("students = " + str(k))
+print("apples = " +  str(n))
+print("students gets" + str(a) + "apple")
+print("only " + str(x) + " left")
